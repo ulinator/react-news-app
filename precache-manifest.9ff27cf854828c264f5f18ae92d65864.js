62beddbab6e9befdb88a42f309fdabd3c3e36ecc
Ulinator <zahuta@gmail.com>
2019-12-13 13:35:06 +0100

    Updates

---
+++ precache-manifest.9ff27cf854828c264f5f18ae92d65864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436b053e628d2acafa31600a7fc6587c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e3c4aa8fc3fad705747",
+    "url": "/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "606d58bfde5c2c901847",
+    "url": "/static/css/main.d3a00c98.chunk.css"
+  },
+  {
+    "revision": "3e3c4aa8fc3fad705747",
+    "url": "/static/js/2.1adf92b3.chunk.js"
+  },
+  {
+    "revision": "606d58bfde5c2c901847",
+    "url": "/static/js/main.c8e3ef3c.chunk.js"
+  },
+  {
+    "revision": "52f0a80b4c90bce311ee",
+    "url": "/static/js/runtime-main.088202f7.js"
+  }
+]);
